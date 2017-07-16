7b90352a8ed6f7e100df39f5f255344fd361cebc
Santosh T <bt.santosh01@gmail.com>
2017-07-16 10:55:42 +0530

    Implemeted enum for status

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-	validates :date, :rationale, presence: true
-
+	enum status: {submitted: 0, approved: 1, rejected: 2}	
 	belongs_to :user
+	validates :date, :rationale, presence: true
 end
